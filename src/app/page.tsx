cccb567e60773fb82b2fd782ad709c026329de91
Dell-Chaves2000 <wendell_chaves2000@yahoo.com.br>
2025-08-17 16:29:50 -0300

    Segundo componete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,31 +13,36 @@ export default Home*/
 type Morador = {
   primeiroNome: string;
   sobrenome: string;
-}
+};
 
 export default function Home() {
-
-
-function formatarNomeMorador(morador:Morador) {
-
-  return morador.primeiroNome + ' ' + morador.sobrenome
-}
-
-const morador = {
-  primeiroNome: 'Sandro',
-  sobrenome: 'Pereira'
-}
-
-function obterSaudacao(morador: null | Morador) {
-  if(morador) {
-    return <span>Olá, {formatarNomeMorador(morador)}!</span>
+  function formatarNomeMorador(morador: Morador) {
+    return morador.primeiroNome + " " + morador.sobrenome;
   }
-  return (<span>Olá, Estranho!!!</span>)
-}
 
-return (
-  <div className="flex flex-col items-center justify-center h-screen">
-    <h1 className="text-4x1 font-bold">{obterSaudacao(morador)}</h1>
-  </div>
-)
+  const morador = {
+    primeiroNome: "Sandro",
+    sobrenome: "Pereira",
+  };
+
+  function obterSaudacao(morador: null | Morador) {
+    if (morador) {
+      return (
+        <div className="border border-blue bg-blue-500 text-white px-4 py-2 rounded-lg">
+          Olá, {formatarNomeMorador(morador)}!
+        </div>
+      );
+    }
+    return (
+      <div className="border border-blue bg-blue-500 text-white px-4 py-2 rounded-lg">
+        Olá, Estranho!!!
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <h1 className="text-4xl font-bold">{obterSaudacao(morador)}</h1>
+    </div>
+  );
 }
